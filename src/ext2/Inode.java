65e664d8e3bb8ca1f6f5f47b96694dbc16f82874
guillelopez22 <guillelopez_13@hotmail.com>
2018-06-02 16:27:30 -0600

    dir->bits dir<-bytes

--- src/ext2/Inode.java
+++ src/ext2/Inode.java
@@ -5,10 +5,13 @@ import com.google.common.primitives.Ints;
 
 import java.nio.ByteBuffer;
 import java.nio.IntBuffer;
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
 
 import static java.lang.Math.toIntExact;
-
 
 public class Inode {
     public static final int DIRECTORY = 0;
@@ -65,7 +68,16 @@ public class Inode {
         return type;
     }
 
-    // Reads 64 bytes from the byte array[] and creates a new instance of Inde from it
+    public int getSize() {
+        return size;
+    }
+
+    // Return epoch time
+    public int getCreationTime() {
+        return creationTime;
+    }
+
+    // Reads 64 bytes from the byte array[] and creates a new instance of Inode from it
     public static Inode fromByteArray(byte array[]) {
         // Split 64 byte array into subarrays
         final byte TYPE[] = Arrays.copyOfRange(array, 0, 4);
@@ -81,9 +93,9 @@ public class Inode {
         int delTime = Ints.fromByteArray(DEL_TIME);
 
         // Create pointers array
-        IntBuffer intBuf = ByteBuffer.wrap(POINTERS).asIntBuffer();
-        int[] pointers = new int[intBuf.remaining()];
-        intBuf.get(pointers);
+        IntBuffer intBuffer = ByteBuffer.wrap(POINTERS).asIntBuffer();
+        int[] pointers = new int[intBuffer.remaining()];
+        intBuffer.get(pointers);
 
         // Create instance and return it
         Inode inode = new Inode(type, size);
